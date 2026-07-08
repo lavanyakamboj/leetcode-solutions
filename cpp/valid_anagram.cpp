5c0156f1786b7089c3e27f3fc3d0e5cd3a296ffe
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-08 21:04:31 +0530

    LeetCode: update cpp/valid_anagram.cpp

--- cpp/valid_anagram.cpp
+++ cpp/valid_anagram.cpp
@@ -3,7 +3,7 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/valid-anagram/
 // Synced by: LinkCode
-// Date: 7/8/2026, 8:58:59 PM
+// Date: 7/8/2026, 9:04:29 PM
 // ======================================
 
 
@@ -17,15 +17,11 @@ public:
         unordered_map<char,int> map1;
         unordered_map<char,int> map2;
         for(int i=0;i<len;i++){
-            if(map1.count(s[i]))
-                map1[s[i]]=map1[s[i]]+1;
-            else
-                map1[s[i]]=1;
-
-            if(map2.count(t[i]))
-                map2[t[i]]=map2[t[i]]+1;
-            else
-                map2[t[i]]=1;
+            // for first map - string s
+                map1[s[i]]++;
+            
+            // for second map string t
+                map2[t[i]]++;
         }
         for(auto i : map1){
             if(map2[i.first] != map1[i.first]){
