615ccb6c63153e039fcbb0f6ff243474598e4dc9
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-08 17:17:18 +0530

    LeetCode: update cpp/contains_duplicate.cpp

--- cpp/contains_duplicate.cpp
+++ cpp/contains_duplicate.cpp
@@ -3,19 +3,18 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/contains-duplicate/
 // Synced by: LinkCode
-// Date: 7/8/2026, 5:14:25 PM
+// Date: 7/8/2026, 5:17:17 PM
 // ======================================
 
 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> map;
-        for(int i=0;i<nums.size();i++){
-            if(map.count(nums[i])){
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
-            map[nums[i]]=1;
         }
         return false;
     }
