e51fb961043e448ff0e614676d90da80e34a31a8
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-10 11:59:35 +0530

    LeetCode: update cpp/ransom_note.cpp

--- cpp/ransom_note.cpp
+++ cpp/ransom_note.cpp
@@ -3,25 +3,49 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/ransom-note/
 // Synced by: LinkCode
-// Date: 7/10/2026, 11:54:53 AM
+// Date: 7/10/2026, 11:59:33 AM
 // ======================================
 
+
+// using two hash maps :    
+
+// class Solution {
+// public:
+//     bool canConstruct(string ransomNote, string magazine) {
+//         unordered_map<char,int> map1;
+//         unordered_map<char,int> map2;
+// hashmap storing frequency for rensom number 
+//         for(char s : ransomNote){
+//             map1[s]++;
+//         }
+// hashmap storing frequency for the magazine
+//         for(char s : magazine){
+//             map2[s]++;
+//         }
+//         for(auto i : map1){
+//             if(i.second > map2[i.first]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+// solution using only one hashmap
 
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int> map1;
-        unordered_map<char,int> map2;
-        for(char s : ransomNote){
-            map1[s]++;
-        }
+        unordered_map<char,int> map;
         for(char s : magazine){
-            map2[s]++;
+            map[s]++;
         }
-        for(auto i : map1){
-            if(i.second > map2[i.first]){
+        for(auto i : ransomNote){
+            if(map[i]==0){
                 return false;
             }
+            map[i]--;
         }
         return true;
     }
