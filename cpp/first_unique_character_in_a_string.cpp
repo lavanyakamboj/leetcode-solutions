4420efb23cac472dc38e6cfdf9472c558dcf6681
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-10 10:56:50 +0530

    LeetCode: update cpp/first_unique_character_in_a_string.cpp

---
+++ cpp/first_unique_character_in_a_string.cpp
@@ -0,0 +1,27 @@
+// ======================================
+// LeetCode Problem: first unique character in a string
+// Language: cpp
+// Link: https://leetcode.com/problems/first-unique-character-in-a-string/
+// Synced by: LinkCode
+// Date: 7/10/2026, 10:56:49 AM
+// ======================================
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> map;
+        for(char str : s){
+            map[str]++;
+        }
+        for(int i=0;i<s.size();i++){
+            char str = s[i];
+            if(map.count(str)){
+                if(map[str]==1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
