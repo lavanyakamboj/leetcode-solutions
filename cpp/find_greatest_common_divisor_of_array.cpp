eb4eaeb45197084d405d8690f1df62e4b03d0b0e
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-18 17:03:22 +0530

    LeetCode: update cpp/find_greatest_common_divisor_of_array.cpp

--- cpp/find_greatest_common_divisor_of_array.cpp
+++ cpp/find_greatest_common_divisor_of_array.cpp
@@ -3,21 +3,27 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/find-greatest-common-divisor-of-array/
 // Synced by: LinkCode
-// Date: 7/18/2026, 5:01:07 PM
+// Date: 7/18/2026, 5:03:22 PM
 // ======================================
 
 
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size()-1;
-        int n1 = nums[0];
-        int n2 = nums[n];
-        while(n2 != 0){
-            n1 = n1 % n2;
-            swap(n1,n2);
+        int smallest = INT_MAX;
+        int largest = INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] >largest){
+                largest = nums[i];
+            }
+            if(nums[i]<smallest){
+                smallest = nums[i];
+            }
         }
-        return n1;
+        while(largest != 0){
+            smallest = smallest % largest;
+            swap(smallest,largest);
+        }
+        return smallest;
     }
 };
