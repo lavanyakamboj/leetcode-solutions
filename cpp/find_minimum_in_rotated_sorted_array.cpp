89ebf7e9cdbbeaa0fdc94a0eb1f74f5c71c90ccf
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-26 21:58:33 +0530

    LeetCode: update cpp/find_minimum_in_rotated_sorted_array.cpp

--- cpp/find_minimum_in_rotated_sorted_array.cpp
+++ cpp/find_minimum_in_rotated_sorted_array.cpp
@@ -3,14 +3,26 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 // Synced by: LinkCode
-// Date: 7/26/2026, 9:37:22 PM
+// Date: 7/26/2026, 9:58:31 PM
 // ======================================
 
 
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[0];
+        int n = nums.size();
+        int left = 0 ;
+        int right = n-1;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] > nums[right]){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return nums[left];
     }
 };
