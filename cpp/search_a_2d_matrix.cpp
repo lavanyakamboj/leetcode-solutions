91cba90e82871909e85fc081a362790e5c75d8bc
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-07-27 21:30:09 +0530

    LeetCode: update cpp/search_a_2d_matrix.cpp

--- cpp/search_a_2d_matrix.cpp
+++ cpp/search_a_2d_matrix.cpp
@@ -3,7 +3,7 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/search-a-2d-matrix/
 // Synced by: LinkCode
-// Date: 7/27/2026, 9:27:18 PM
+// Date: 7/27/2026, 9:30:07 PM
 // ======================================
 
 
@@ -15,10 +15,17 @@ public:
 
         for(int i=0 ; i< row ; i++){
             if(matrix[i][col-1] >= target){
-                for(int j=0 ; j< col;j++){
-                    if(matrix[i][j] == target){
+                int left = 0;
+                int right = col - 1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+
+                    if (matrix[i][mid] == target)
                         return true;
-                    }
+                    else if (matrix[i][mid] < target)
+                        left = mid + 1;
+                    else
+                        right = mid - 1;
                 }
                  return false; 
             }
