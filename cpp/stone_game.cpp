9ade7161af20140505d954d9354e6f1641bb9f0b
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-08-02 16:57:29 +0530

    LeetCode: update cpp/stone_game.cpp

---
+++ cpp/stone_game.cpp
@@ -0,0 +1,15 @@
+// ======================================
+// LeetCode Problem: stone game
+// Language: cpp
+// Link: https://leetcode.com/problems/stone-game/
+// Synced by: LinkCode
+// Date: 8/2/2026, 4:57:28 PM
+// ======================================
+
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
