2bb2abd31f8e5d94d021c6a014cf81a4b66997d3
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-08-25 21:55:26 +0530

    LeetCode: update cpp/smallest_missing_multiple_of_k.cpp

--- cpp/smallest_missing_multiple_of_k.cpp
+++ cpp/smallest_missing_multiple_of_k.cpp
@@ -3,26 +3,46 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/smallest-missing-multiple-of-k/
 // Synced by: LinkCode
-// Date: 8/25/2026, 8:32:58 PM
+// Date: 8/25/2026, 9:55:26 PM
 // ======================================
 
+
+// unordered set approach  
+
+// class Solution {
+// public:
+//     int missingMultiple(vector<int>& nums, int k) {
+//         unordered_set<int> set;
+//         for(int n : nums){
+//             set.insert(n);
+//         }
+//         int ans=0;
+//         int c =1;
+//         while(true){
+//             if(!set.count(c * k )){
+//                 ans =c*k;
+//                 return ans;
+//             }
+//             c++;
+//         }
+        
+//     }
+// };
+
+
+// soarting approach 
 
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> set;
-        for(int n : nums){
-            set.insert(n);
-        }
-        int ans=0;
-        int c =1;
-        while(true){
-            if(!set.count(c * k )){
-                ans =c*k;
-                return ans;
-            }
-            c++;
-        }
+        sort(nums.begin(), nums.end());
         
+        int c =1;
+        for(auto n : nums){
+            if(n == c * k ){
+                c++;
+            }
+        }
+        return c*k;
     }
 };
