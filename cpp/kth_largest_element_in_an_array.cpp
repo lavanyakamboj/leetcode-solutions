1e4c3594a7349127ef9ae9bd23d8bee3fa3f16c3
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-08-28 20:38:49 +0530

    LeetCode: update cpp/kth_largest_element_in_an_array.cpp

--- cpp/kth_largest_element_in_an_array.cpp
+++ cpp/kth_largest_element_in_an_array.cpp
@@ -3,14 +3,37 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/kth-largest-element-in-an-array/
 // Synced by: LinkCode
-// Date: 8/28/2026, 5:47:37 PM
+// Date: 8/28/2026, 8:38:45 PM
 // ======================================
 
+
+// ----- sorting solution -----
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.rbegin(), nums.rend());
+//         return nums[k-1];
+//     }
+// };
+
+// ---- hash map ----
 
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.rbegin(), nums.rend());
-        return nums[k-1];
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int n : nums){
+            if(pq.size()<k){
+                pq.push(n);
+            }
+            else{
+                if(n > pq.top()){
+                    pq.pop();
+                    pq.push(n);
+                }
+            }
+        }
+        return pq.top();
     }
 };
