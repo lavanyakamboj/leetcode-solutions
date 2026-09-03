4860434da86bd9baef9298bb6a98c64942847ce6
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-03 21:50:48 +0530

    LeetCode: update cpp/permutations.cpp

---
+++ cpp/permutations.cpp
@@ -0,0 +1,31 @@
+// ======================================
+// LeetCode Problem: permutations
+// Language: cpp
+// Link: https://leetcode.com/problems/permutations/
+// Synced by: LinkCode
+// Date: 9/3/2026, 9:50:45 PM
+// ======================================
+
+
+class Solution {
+public:
+    // backtracking function
+    void backrtacking(vector<int>& nums, int index,vector<vector<int>>& ans){
+        if(index == nums.size()){
+               ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
+            backrtacking(nums,index + 1,ans);
+            swap(nums[i],nums[index]);
+        }
+    }
+
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        backrtacking(nums,0,ans);
+        return ans;
+    }
+};
