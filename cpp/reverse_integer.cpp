6442b8fbf5b46ae74e51fcb97fef9bb0877b6f50
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-07 21:58:08 +0530

    LeetCode: update cpp/reverse_integer.cpp

--- cpp/reverse_integer.cpp
+++ cpp/reverse_integer.cpp
@@ -3,13 +3,22 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/reverse-integer/
 // Synced by: LinkCode
-// Date: 9/7/2026, 9:57:59 PM
+// Date: 9/7/2026, 9:58:09 PM
 // ======================================
 
 
 class Solution {
 public:
     int reverse(int x) {
-        
+        int num=0;
+        while(x!=0){
+            int rem=x%10;
+            x=x/10;
+            if((num>INT_MAX/10) || (num<INT_MIN/10)){
+                return 0;
+            }
+            num=(num*10)+rem;
+        }
+        return num;
     }
 };
