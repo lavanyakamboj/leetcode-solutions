c3eb2f1e2f1ad11260090c750bd05556e4a5648e
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-09 22:36:01 +0530

    LeetCode: update cpp/two_sum.cpp

--- cpp/two_sum.cpp
+++ cpp/two_sum.cpp
@@ -3,7 +3,7 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/two-sum/
 // Synced by: LinkCode
-// Date: 9/7/2026, 9:57:39 PM
+// Date: 9/9/2026, 10:36:00 PM
 // ======================================
 
 
@@ -12,11 +12,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> map;
         for(int i=0;i<nums.size();i++){
-            if(map.count(target-nums[i])){
-                return{map[target-nums[i]],i};
+            if(map.count(target - nums[i])){
+                return {map[target-nums[i]], i};
             }
             map[nums[i]]=i;
         }
-        return{};
+    return {0,0};
     }
 };
