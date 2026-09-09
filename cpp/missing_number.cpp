08ceb79e1dcd3e96bdb320f4b2304e13831be377
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-09 22:17:00 +0530

    LeetCode: update cpp/missing_number.cpp

--- cpp/missing_number.cpp
+++ cpp/missing_number.cpp
@@ -3,22 +3,38 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/missing-number/
 // Synced by: LinkCode
-// Date: 9/9/2026, 9:41:30 PM
+// Date: 9/9/2026, 10:16:59 PM
 // ======================================
 
+
+//  idea --- ans =  expected sum - actual sum 
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         long long sum=0;
+//         long long numsum = 0;
+//         for(int i=0;i<=n;i++){
+//             sum = sum + i;
+//         }
+//         for(int i=0;i<n;i++){
+//             numsum = numsum + nums[i];
+//         }
+//         return sum - numsum;
+//     }
+// };
+
+
+//  XOR method
 
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        long long sum=0;
-        long long numsum = 0;
-        for(int i=0;i<=n;i++){
-            sum = sum + i;
-        }
+        int XOR =0;
         for(int i=0;i<n;i++){
-            numsum = numsum + nums[i];
+            XOR = XOR ^ nums[i] ^ i;
         }
-        return sum - numsum;
+        return XOR^n;
     }
 };
