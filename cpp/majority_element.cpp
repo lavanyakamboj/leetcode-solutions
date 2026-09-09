6adcc0e0cc2ae1e8bda34a9e3da3b9a680d803a5
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-09 22:50:02 +0530

    LeetCode: update cpp/majority_element.cpp

--- cpp/majority_element.cpp
+++ cpp/majority_element.cpp
@@ -3,24 +3,46 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/majority-element/
 // Synced by: LinkCode
-// Date: 9/9/2026, 10:46:56 PM
+// Date: 9/9/2026, 10:50:01 PM
 // ======================================
 
+
+// -- using unordered_map--
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int,int> map;
+//         for(int n : nums){
+//             map[n]++;
+//         }
+//         int max=0;
+//         int ans = 0;
+//         for(auto i : map){
+//             if(i.second> max){
+//                 max=i.second;
+//                 ans=i.first;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// -- best solution --
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> map;
-        for(int n : nums){
-            map[n]++;
-        }
-        int max=0;
+        int count =0;
         int ans = 0;
-        for(auto i : map){
-            if(i.second> max){
-                max=i.second;
-                ans=i.first;
-            }
+        for(int n : nums){
+            if(count == 0)
+                ans = n;
+            
+            if(ans == n)
+                count++;
+            else
+                count--;
         }
         return ans;
     }
