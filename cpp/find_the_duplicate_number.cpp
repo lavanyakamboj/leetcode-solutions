face2091b89eccf082a4aaaa2abdd2080bfa3799
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-09 22:28:05 +0530

    LeetCode: update cpp/find_the_duplicate_number.cpp

--- cpp/find_the_duplicate_number.cpp
+++ cpp/find_the_duplicate_number.cpp
@@ -3,20 +3,36 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/find-the-duplicate-number/
 // Synced by: LinkCode
-// Date: 9/9/2026, 10:23:13 PM
+// Date: 9/9/2026, 10:28:04 PM
 // ======================================
 
+
+// solution using unordered set -- (problem - extra soace)
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//     unordered_set<int> set;
+//     for(int n : nums){
+//         if(set.count(n)){
+//             return n ;
+//         }
+//         set.insert(n);
+//     }
+//     return 0;
+//     }
+// };
+
+// without extra space
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-    unordered_set<int> set;
-    for(int n : nums){
-        if(set.count(n)){
-            return n ;
+        sort(nums.begin(), nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])
+                return nums[i];
         }
-        set.insert(n);
-    }
-    return 0;
+        return 0;
     }
 };
