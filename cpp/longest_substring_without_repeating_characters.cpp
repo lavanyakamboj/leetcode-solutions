493040593e771159283cb9ea14c391dccdf75d71
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-17 22:50:44 +0530

    LeetCode: update cpp/longest_substring_without_repeating_characters.cpp

--- cpp/longest_substring_without_repeating_characters.cpp
+++ cpp/longest_substring_without_repeating_characters.cpp
@@ -3,22 +3,30 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Synced by: LinkCode
-// Date: 7/20/2026, 8:26:58 PM
+// Date: 9/17/2026, 10:50:43 PM
 // ======================================
 
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int left =0, maxlen=0;
-        unordered_map<char,int> map;
-        for(int right=0; right<s.length(); right++){
-            if(map.count(s[right])){
-                left=max(map[s[right]]+1,left);
+        int left = 0;
+        int ans = 0;
+
+        unordered_set<char> map;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            while (map.count(s[right])) {
+                map.erase(s[left]);
+                left++;
             }
-            map[s[right]]= right;
-            maxlen=max(maxlen, right-left+1);
+
+            map.insert(s[right]);
+
+            ans = max(ans, right - left + 1);
         }
-        return maxlen;
+
+        return ans;
     }
 };
