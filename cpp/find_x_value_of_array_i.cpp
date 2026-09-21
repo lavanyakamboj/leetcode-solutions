ab8748f66207377ad5272d9a3136b4f42ab8d8df
LavanyaKamboj <kambojlavanya076@gmail.com>
2026-09-21 21:31:24 +0530

    LeetCode: update cpp/find_x_value_of_array_i.cpp

---
+++ cpp/find_x_value_of_array_i.cpp
@@ -0,0 +1,33 @@
+// ======================================
+// LeetCode Problem: find x value of array i
+// Language: cpp
+// Link: https://leetcode.com/problems/find-x-value-of-array-i/
+// Synced by: LinkCode
+// Date: 9/21/2026, 9:31:23 PM
+// ======================================
+
+
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& A, int k) {
+        vector<long long> res(k);
+        int freq[5] = {0};
+
+        for (auto& n : A) {
+            n %= k;
+            int cur[5] = {0}; //current freq.
+
+            cur[n] = 1;
+
+            for (int x = 0; x < k; x++)
+                cur[x * n % k] += freq[x];
+
+            for (int x = 0; x < k; x++) {
+                freq[x] = cur[x];
+                res[x] += freq[x];
+            }
+        }
+
+        return res;
+    }
+};
